52d86265fefd419318416207cd59ac7b63f789c4
Ilse Macías <ilse.macias@gmail.com>
2018-01-30 11:05:37 -0800

    Exercise Guru99 page using class. Methods: VerifyAndSortByName() and VerifyCostOfProductInListAndDetails()

---
+++ Guru99/MobileProducts.cs
@@ -0,0 +1,49 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Support.UI;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Guru99
+//{
+//    [TestClass]
+//    public class MobileProducts
+//    {
+//        private IWebDriver _driver;
+//        private ChromeOptions _chromeOptions;
+//        private WebDriverWait _wait;
+
+//        [TestInitialize]
+//        public void Setup()
+//        {
+//            _chromeOptions = new ChromeOptions();
+
+//            _driver = new ChromeDriver(_chromeOptions);
+//            _driver.Manage().Cookies.DeleteAllCookies();
+//            _driver.Navigate().GoToUrl("http://live.guru99.com/index.php/");
+//            _driver.Manage().Window.Maximize();
+//            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
+//        }
+
+//        [TestMethod]
+//        [TestCategory("Read the cost of Sony Xperia mobile")]
+//        public void CostOfSonyXperiaMobile()
+//        {
+//            string priceSonyXperia = _driver.FindElement(By.Id("product-price-1"))
+//                .Text;
+//            Assert.AreEqual("$100.00", priceSonyXperia);
+//            Console.WriteLine("Product Value in list and details pagre should be equal " + priceSonyXperia + " dlls.");
+//        }
+
+//        [TestCleanup]
+//        public void TearDown()
+//        {
+//            _driver.Close();
+//            _driver.Quit();
+//        }
+//    }
+//}
---
+++ Guru99/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Guru99")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Guru99")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9110b7d4-8ca6-455a-a47d-1c4416bc9b6d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
