8c67d03255ba089bfb5ad8789298b4c4d3186f24
Ilse Macías <ilse.macias@gmail.com>
2018-01-31 10:50:56 -0800

    Cart module.

--- Guru99/Cart.cs
+++ Guru99/Cart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Guru99
 {
+    [TestClass]
     public class Cart
     {
+        [TestMethod]
+        public void AddToCartButton()
+        {
+            
+        }
     }
 }
